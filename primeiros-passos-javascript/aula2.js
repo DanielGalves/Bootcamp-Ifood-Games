e1c223b71ff212a218478e3f2f040631164b529c
Daniel Galves <danielgama1@live.com>
2023-11-16 18:45:35 -0300

    Primeiros passos com JavaScript - Aula2: Trabalhando com IF e Else

--- primeiros-passos-javascript/aula2.js
+++ primeiros-passos-javascript/aula2.js
@@ -1,4 +1,11 @@
-const numero = 15;
+const numero = 10;
 const numeroPar = (numero % 2) === 0;
 
-console.log(numeroPar);
+if(numeroPar){
+    console.log('O numero ' + numero + ' é um numero par!!!');
+    
+}else{
+    
+    console.log('O numero ' + numero + ' é um numero impar!!!');
+}
+
