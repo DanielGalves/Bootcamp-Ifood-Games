0f9906b49a9df8da0bd8cea36440bdc90cfd050f
Daniel Galves <danielgama1@live.com>
2023-11-16 19:34:42 -0300

    Primeiros passos com JavaScript - Aula2: Melhorando a Calculadora de Gastos com viagem

---
+++ primeiros-passos-javascript/calculadoraViagem.js
@@ -0,0 +1,15 @@
+var gasolina = 5.79;
+var etanol = 3.65;
+var kmPorLitros = 14;
+var distancia = 1000;
+var tipoCombustivel = 'gasolina';
+
+if(tipoCombustivel === 'gasolina'){
+    var valorGasto = (distancia/kmPorLitros) * gasolina;
+    console.log('R$' + valorGasto.toFixed(2));
+}else if(tipoCombustivel === 'etanol'){
+    var valorGasto = (distancia/kmPorLitros) * etanol;
+    console.log('R$' + valorGasto.toFixed(2));
+}else{
+    console.log('Combustível inválido');
+}
