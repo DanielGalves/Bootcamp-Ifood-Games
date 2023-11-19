093d01ae9ccf8330dda7841e9b01442e48f71d67
Daniel Galves <danielgama1@live.com>
2023-11-19 15:25:02 -0300

    Refatorando o codigo co calculo d IMC utilizando functions

--- primeiros-passos-javascript/calculoImc.js
+++ primeiros-passos-javascript/calculoImc.js
@@ -1,17 +1,27 @@
-
-const altura = 1.65;
-const peso = 70;
-
-var imc = peso / Math.pow(altura, 2);
-
-if (imc < 18.5) {
-    console.log('Abaixo do peso');
-} else if (imc >= 18.5 && imc < 25) {
-    console.log('Peso normal');
-} else if (imc >= 25 && imc < 30) {
-    console.log('Acima do peso');
-} else if (imc >= 30 && imc < 40) {
-    console.log('Obeso');
-} else {
-    console.log('Obesidade Grave');
+function calculoImc(peso,altura){
+    return peso / Math.pow(altura, 2);
 }
+function classificarImc(imc){
+    if (imc < 18.5) {
+        return 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 25) {
+        return 'Peso normal';
+    } else if (imc >= 25 && imc < 30) {
+        return 'Acima do peso';
+    } else if (imc >= 30 && imc < 40) {
+        console.log('Obeso');
+    } else {
+        return 'Obesidade Grave';
+    }
+
+}
+
+function main(){
+    const altura = 1.65;
+    const peso = 70;
+
+    var imc = calculoImc(peso,altura);
+    console.log(classificarImc(imc));
+}
+
+main();
