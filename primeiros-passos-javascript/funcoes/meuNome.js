3b88a98f1b94548950fbcaf3391a1d0f706f6c81
Daniel Galves <danielgama1@live.com>
2023-11-19 17:11:14 -0300

    Exercicio Funções : Incrementado função para verificar a idade

--- primeiros-passos-javascript/funcoes/meuNome.js
+++ primeiros-passos-javascript/funcoes/meuNome.js
@@ -1,8 +1,19 @@
 function escrevaNome(nome){
     return nome;
-}
+};
+
+function verificaIdade(idade){
+    if(idade < 18){
+        return 'Menor de idade';
+    }else {
+        return 'Maior de idade';
+    }
+};
+
 
 (function (){
     const nome = 'Daniel';
-    console.log("Meu nome é " + escrevaNome(nome));
+    const idade = 17;
+    console.log("Meu nome é " + escrevaNome(nome) +
+                "\nVocê é " + verificaIdade(idade)   );
 })();
