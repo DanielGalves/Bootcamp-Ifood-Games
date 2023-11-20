4970aa77948132130cfe7a33b1adcd7b03ae6167
Daniel Galves <danielgama1@live.com>
2023-11-20 11:30:35 -0300

    Criando metodos para o objeto

--- primeiros-passos-javascript/classes-objetos/objeto.js
+++ primeiros-passos-javascript/classes-objetos/objeto.js
@@ -9,7 +9,22 @@ const pessoa = {
 }
 
 
+
+
 pessoa.altura = 1.65;
 
 
 pessoa.descrever();
+
+console.log(pessoa);
+
+pessoa['altura'] = 1.63;
+
+console.log(pessoa);
+
+const atributo = 41;
+
+pessoa.idade = atributo;
+
+console.log(pessoa);
+
