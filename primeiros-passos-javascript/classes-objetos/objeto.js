6810febab3d8830f95570b28ec74435e3693c4d0
Daniel Galves <danielgama1@live.com>
2023-11-20 12:05:03 -0300

    Funções recebendo objetos

--- primeiros-passos-javascript/classes-objetos/objeto.js
+++ primeiros-passos-javascript/classes-objetos/objeto.js
@@ -16,10 +16,24 @@ class Pessoa{
 }
 
 const daniel = new Pessoa('Daniel', 40);
-const alice = new Pessoa('Alice', 10);
+const alice = new Pessoa('Alice', 40);
 
 
 daniel.descrever();
 alice.descrever();
 console.log(daniel);
 console.log(alice);
+
+function compararIdade(p1,p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho que ${p2.nome}`);
+    }else if (p1.idade < p2.idade){
+        console.log(`${p2.nome} é mais velho que ${p1.nome}`);
+    }else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+
+    }
+}
+
+
+compararIdade(daniel,alice);
