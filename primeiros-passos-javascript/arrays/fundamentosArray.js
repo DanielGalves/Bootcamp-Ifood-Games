de1a2259ce64412e43cdec75404504a40212188c
Daniel Galves <danielgama1@live.com>
2023-11-21 14:58:37 -0300

    Refatorando o codigo de fundamentosArray

--- primeiros-passos-javascript/arrays/fundamentosArray.js
+++ primeiros-passos-javascript/arrays/fundamentosArray.js
@@ -1,8 +1,22 @@
 
-const nome = 'Daniel Gama Alves';
-
-for (let i = 0; i < nome.length; i++) {
-    const letra = nome[i];
-    console.log(letra);
+function imprimirLetras(){
     
+    const nome = 'Daniel Gama Alves';
+    
+    for (let i = 0; i < nome.length; i++) {
+        const letra = nome[i];
+        console.log(letra);
+        
+    }
+}
+
+function adicionarNotas(){
+    const notas = [];
+
+    notas.push[5];
+    notas.push[7];
+    notas.push[8];
+    notas.push[2];
+    notas.push[5];
+    notas.push[8];
 }
