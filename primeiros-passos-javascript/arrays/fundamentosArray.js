40f92793f44039217e2fc28055dd1c9f2771c4a1
Daniel Galves <danielgama1@live.com>
2023-11-21 15:06:03 -0300

    Refatorando o codigo de fundamentosArray

--- primeiros-passos-javascript/arrays/fundamentosArray.js
+++ primeiros-passos-javascript/arrays/fundamentosArray.js
@@ -13,10 +13,15 @@ function imprimirLetras(){
 function adicionarNotas(){
     const notas = [];
 
-    notas.push[5];
-    notas.push[7];
-    notas.push[8];
-    notas.push[2];
-    notas.push[5];
-    notas.push[8];
+    notas.push(5);
+    notas.push(7);
+    notas.push(8);
+    notas.push(2);
+    notas.push(5);
+    notas.push(8);
+
+    console.log(notas);
 }
+
+imprimirLetras();
+adicionarNotas();
