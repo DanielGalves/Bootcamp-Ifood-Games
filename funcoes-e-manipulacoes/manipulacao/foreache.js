acd0f7e970245dc94019b2e23dbf55a7afb2f7f8
Daniel Galves <danielgama1@live.com>
2023-12-01 23:19:02 -0300

    aprendendo a manipular Arrays com foreach

---
+++ funcoes-e-manipulacoes/manipulacao/foreache.js
@@ -0,0 +1,14 @@
+
+
+const lista = [1,2,3,4,5,6,7,8,9,10];
+
+// imprime o valor, o indice e a lista de referencia dos valores e do indice
+//lista.forEach((value, i, listRef) => {
+//    console.log(value + i , listRef)
+//}  )
+
+
+//percorre toda a lista e imprime cada valor
+lista.forEach((value) => {
+    console.log(value)
+})
