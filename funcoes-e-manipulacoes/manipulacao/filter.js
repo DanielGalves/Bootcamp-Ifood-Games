7f61464a41c96ea2cbfb3950a3657608beb034ba
Daniel Galves <danielgama1@live.com>
2023-12-01 23:34:57 -0300

    entendendo o filter e a diferença para o ForEach

---
+++ funcoes-e-manipulacoes/manipulacao/filter.js
@@ -0,0 +1,19 @@
+
+
+const lista = [1,2,3,4,5,6,7,8,9];
+
+
+// o forEach mostra um valor por vez
+const listaDeParesForEach = lista.forEach((value) => {
+    return(value % 2 === 0)
+})
+
+//com o filter e retornado nova lista
+const listaDeParesFilter = lista.filter((value) =>{
+    return (value % 2 === 0)
+})
+
+console.log(listaDeParesForEach)
+console.log(listaDeParesFilter)
+
+
