d78be28e3c4c15abec5cda3107f60107f157b90e
Daniel Galves <danielgama1@live.com>
2023-12-26 21:48:11 -0300

    Adicionando classes dinamicamente

--- game-piano/src/scripts/engine.js
+++ game-piano/src/scripts/engine.js
@@ -1,19 +1,34 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mapedKey = [];
+
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) =>{
     console.log(key.dataset.key);
     
     key.addEventListener("click", () => playTune(key.dataset.key));
+
+    mapedKey.push(key.dataset.key);
 })
 
 document.addEventListener("keydown", (e) =>{
-    console.log(e.key);
-    playTune(e.key);
+    
+    if (mapedKey.includes(e.key)){
+        playTune(e.key);
+
+    }
+    
 })
